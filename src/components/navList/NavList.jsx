b49d5dbc566d9e73ede88ebada68be2b26891a6d
yousefn <jonabilmc@gmail.com>
2024-10-08 03:04:10 +0300

    navlist

--- src/components/navList/NavList.jsx
+++ src/components/navList/NavList.jsx
@@ -4,10 +4,17 @@ import "./style/navList.css";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleCloseNavList } from "@/state/features/navList/navListSlice";
+import { useRouter } from "next/navigation";
 
 const NavList = () => {
   const isOpen = useSelector((store) => store.navList.isOpen);
   const dispatch = useDispatch();
+  const router = useRouter();
+  function handleClicked(e, link) {
+    e.preventDefault();
+    router.push(link);
+    dispatch(toggleCloseNavList());
+  }
   return (
     <div
       className={`nav-list block lg:hidden fixed inset-0 w-screen z-50  bg-white h-full ${
@@ -15,7 +22,9 @@ const NavList = () => {
       }`}
     >
       <div className="nav-list-header flex justify-between items-center border-b p-4">
-        <Link href="/"><h3 className="text-primary font-bold text-2xl">RSAY</h3></Link>
+        <Link href="/">
+          <h3 className="text-primary font-bold text-2xl">RSAY</h3>
+        </Link>
         <button
           onClick={() => dispatch(toggleCloseNavList())}
           className="text-primary"
@@ -36,22 +45,28 @@ const NavList = () => {
       <nav>
         <ul>
           <li>
-            <Link href="/">
+            <Link onClick={(e) => handleClicked(e, "/")} href="/">
               <span>Home</span>
             </Link>
           </li>
           <li>
-            <Link href="/about">
+            <Link onClick={(e) => handleClicked(e, "/about")} href="/about">
               <span>About us</span>
             </Link>
           </li>
           <li>
-            <Link href="/services">
+            <Link
+              onClick={(e) => handleClicked(e, "/services")}
+              href="/services"
+            >
               <span>Our services</span>
             </Link>
           </li>
           <li>
-            <Link href="/cyberSecurityServices">
+            <Link
+              onClick={(e) => handleClicked(e, "/cyberSecurityServices")}
+              href="/cyberSecurityServices"
+            >
               <span>cyber security services</span>
             </Link>
           </li>
